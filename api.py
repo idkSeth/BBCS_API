7bc739f611a0d4f28c9ffd2929b99b7f661144af
Seth <setholav@gmail.com>
2022-05-18 00:24:54 +0800

    teachers and students

--- api.py
+++ api.py
@@ -14,14 +14,28 @@ def students():
     db = sqlite3.connect("people.db")
     cursor = db.cursor()
     cursor.execute("SELECT studentId, name, classId FROM students")
-    data = tuple(cursor.fetchall())
+    data = list(cursor.fetchall())
     db.close()
-    response = flask.make_response(flask.jsonify({"data": data}),200)
-    return response
+    data2 = []
+    for i in data:
+        data2.append({"studentId":i[0], "name":i[1], "classId":i[2]})
+    return flask.make_response({"students":tuple(data2)},200)
 
 @app.route("/student_photo",methods=["GET"])
 def student_photo():
-    pass
+    try:
+        id = flask.request.headers['ID']
+    except KeyError:
+        return flask.make_response("ID not passed in request header",400)
+    else:
+        db = sqlite3.connect("people.db")
+        cursor = db.cursor()
+        cursor.execute("SELECT image FROM students WHERE studentId = (?)", tuple(id))
+        photo = tuple(cursor.fetchall())
+        db.close()
+        return flask.make_response({"photo":photo},200)
+    
+        
     
 
 @app.route("/teachers", methods=["GET"])
@@ -31,8 +45,10 @@ def teachers():
     cursor.execute("SELECT teacherId, name FROM teachers")
     data = tuple(cursor.fetchall())
     db.close()
-    response = flask.make_response(flask.jsonify({"data": data}),200)
-    return response
+    data2 = []
+    for i in data:
+        data2.append({"teacherId":i[0], "name":i[1]})
+    return flask.make_response({"teachers": data2},200)
 
 @app.route("/teacher_photo",methods=["GET"])
 def teacher_photo():
